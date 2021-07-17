cad8d7b2be29e41a0776148071f334118ee608f9
amontalvof <amontalvof92@gmail.com>
2021-07-17 08:58:53 -0500

    feat: Custom Middleware - validateFields

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,16 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const createUser = async (req, res = response) => {
+    const { email, password, username } = req.body;
     res.json({
         ok: true,
         msg: 'register',
+        email,
+        password,
+        username,
     });
 };
 const login = async (req, res = response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped(),
-        });
-    }
     const { email, password } = req.body;
     res.json({
         ok: true,
--- router/auth.js
+++ router/auth.js
@@ -5,11 +5,21 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { createUser, login, renewToken } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate-fields');
 
 const router = Router();
 
 // create new users
-router.post('/new', createUser);
+router.post(
+    '/new',
+    [
+        check('email', 'The email is required').isEmail(),
+        check('password', 'The password is required').not().isEmpty(),
+        check('username', 'The username is required').not().isEmpty(),
+        validateFields,
+    ],
+    createUser
+);
 
 // login
 router.post(
@@ -17,6 +27,7 @@ router.post(
     [
         check('email', 'The email is required').isEmail(),
         check('password', 'The password is required').not().isEmpty(),
+        validateFields,
     ],
     login
 );
