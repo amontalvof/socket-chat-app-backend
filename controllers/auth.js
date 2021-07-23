92e6c109da5916e9ceb49283e08bda5f8fdbf447
amontalvof <amontalvof92@gmail.com>
2021-07-23 07:43:20 -0500

    feat: User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,13 +34,38 @@ const createUser = async (req, res = response) => {
     }
 };
 const login = async (req, res = response) => {
-    const { email, password } = req.body;
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password,
-    });
+    try {
+        const { email, password } = req.body;
+        // Check if the mail exists
+        const userDB = await User.findOne({ email})
+        if (!userDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Email not found in the database',
+            });
+        }
+        // Validate the password
+        const validPassword = bcrypt.compareSync(password, userDB.password)
+        if (!validPassword) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'The password is wrong',
+            });
+        }
+        // Generate Json Web Token
+        const token = await generateJWT(userDB.id)
+        return res.json({
+            ok: true,
+            user: userDB,
+            token,
+        });
+    } catch (error) {
+        console.log(colors.brightMagenta(error));
+        return res.status(500).json({
+            ok: false,
+            msg: 'Talk to the administrator',
+        });
+    }
 };
 const renewToken = async (req, res = response) => {
     res.json({
