2bd1fa90b632ec30899cd239c229d48ab23b0d49
amontalvof <amontalvof92@gmail.com>
2021-08-14 09:08:17 -0500

    feat: Identify connected person - Backend

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,4 @@
+const { verifyJWT } = require('../helpers/jsonWebToken');
 class Sockets {
     constructor(io) {
         this.io = io;
@@ -7,7 +8,12 @@ class Sockets {
     socketsEvents() {
         // On connection
         this.io.on('connection', (socket) => {
-            console.log('Client connected!', socket.id);
+            const [valid, uid] = verifyJWT(socket.handshake.query['x-token']);
+            if (!valid) {
+                console.log('socket no identificado');
+                return socket.disconnect();
+            }
+            console.log('Client connected!', uid);
             // TODO: Validar el JWT_KEY
             // si el token no es valido desconectarlo
 
@@ -22,6 +28,9 @@ class Sockets {
 
             // TODO: disconnect
             // marcar en la bd que el usuario se desconecto
+            socket.on('disconnect', () => {
+                console.log('Client disconnected!', uid);
+            });
 
             // TODO: emitir todos los usuarios conectados
         });
