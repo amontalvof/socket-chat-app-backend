88e9711ced11eb17149d8c6b9c69a5dcefccff88
amontalvof <amontalvof92@gmail.com>
2021-08-23 07:30:03 -0500

    feat: Listen to personal messages

--- models/sockets.js
+++ models/sockets.js
@@ -14,33 +14,31 @@ class Sockets {
     socketsEvents() {
         // On connection
         this.io.on('connection', async (socket) => {
+            // * Validate the JWT_KEY, if the token is not valid disconnect it
             const [valid, uid] = verifyJWT(socket.handshake.query['x-token']);
             if (!valid) {
                 console.log('socket no identificado');
                 return socket.disconnect();
             }
 
+            // * Know which user is active through the uid
             await userConnected(uid);
-            // join user to socket.io room
+
+            // * join user to socket.io room
             socket.join(uid);
-            // TODO: Validar el JWT_KEY
-            // si el token no es valido desconectarlo
 
-            // TODO: Saber que usuario esta activo mediante el uid
-
-            // TODO: Emitir todos los usuarios conectados
+            // * emit all connected users
             this.io.emit('list-users', await getUsers());
 
-            // TODO:  Socket join
-
-            // TODO: escuchar cuando el cliente manda un mensaje
+            // * listen when the user sends a message
             socket.on('personal-message', async (payload) => {
                 const message = await saveMessage(payload);
+                this.io.to(payload.to).emit('personal-message', message);
+                this.io.to(payload.from).emit('personal-message', message);
                 console.log(message);
             });
 
-            // TODO: disconnect
-            // marcar en la bd que el usuario se desconecto
+            // * disconnect, mark in the database that the user disconnected
             socket.on('disconnect', async () => {
                 await userDisconnected(uid);
                 this.io.emit('list-users', await getUsers());
